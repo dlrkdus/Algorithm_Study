8878b37cf81cb0d2886827b8bd3388bcca936789
leegayeon <99721126+dlrkdus@users.noreply.github.com>
2023-12-30 17:23:46 +0900

    수정

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,28 +1,29 @@
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(10000) #파이썬에서 재귀 한도를 풀어준다.
 
-def dfs(x, y):
+def dfs(x,y):
     dx = [0, 0, -1, 1]
     dy = [1, -1, 0, 0]
-    
-    for i in range(4):
+    for i in range(4): #상하좌우 4가지 경우 따지기
         nx = x + dx[i]
         ny = y + dy[i]
         if (0 <= nx < m) and (0 <= ny < n):
-            if field[ny][nx] == 1:
-                field[ny][nx] = 0
+            if matrix[nx][ny] == 1:
+                matrix[nx][ny] = 0 #방문했다면 0으로 초기화 (visited)
                 dfs(nx, ny)
-                
-for _ in range(int(sys.stdin.readline())):
-    m, n, k = map(int, sys.stdin.readline().split())
-    field = [[0 for _ in range(m)] for _ in range(n)]
-    count = 0
+
+t=int(input())
+for _ in range(t):
+    count=0
+    m, n, k = map(int, input().split())
+    matrix = [[0 for _ in range(n)] for _ in range(m)] #m은 행 n은 열
     for _ in range(k):
-        x, y = map(int, sys.stdin.readline().split())
-        field[y][x] = 1
-    for x in range(m):
-        for y in range(n):
-            if field[y][x] == 1:
-                dfs(x, y)
-                count += 1
+        c, r= map(int, input().split())
+        matrix[c][r]=1
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j]==1:
+                dfs(i,j)
+                count+=1
     print(count)
+
