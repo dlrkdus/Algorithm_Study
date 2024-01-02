f6c2e7d85e2da9526a48a4efacfb68dad113cac0
dlrkdus <99721126+dlrkdus@users.noreply.github.com>
2024-01-03 00:04:38 +0900

    [Silver I] Title: Z, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,10 +1,19 @@
-N, r, c = map(int, input().split())
 
-def sol(N, r, c):
+n,r,c=map(int,input().split())
 
-	if N == 0:
-		return 0
-        
-	return 2*(r%2)+(c%2) + 4*sol(N-1, int(r/2), int(c/2))
+def Z(n,r,c): 
+    if n==0:
+        return 0
+    size = 2 ** (n-1)
+    if r<size and c<size: #1사분면
+        return Z(n-1,r,c)
+    elif r>=size and c<size: #2사분면
+        return 2**(2*(n-1))+Z(n-1,r-size,c)
+    elif r<size and c>=size: #3사분면
+        return 2**(2*(n-1))*2+Z(n-1,r,c-size)
+    else: #4사분면
+        return 2**(2*(n-1))*3+Z(n-1,r-size,c-size)
+    
 
-print(sol(N, r, c))
+print(Z(n,c,r))
+      
