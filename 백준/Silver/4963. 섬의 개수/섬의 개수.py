bc72a26f048cf71299ef2e841a2d61dd6f1c753e
dlrkdus <99721126+dlrkdus@users.noreply.github.com>
2024-01-17 15:26:38 +0900

    [Silver II] Title: 섬의 개수, Time: 96 ms, Memory: 34116 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,6 +1,6 @@
 import sys
-sys.setrecursionlimit(10000)
-
+sys.setrecursionlimit(10000) #dfs, bfs 문제에선 반드시 사용
+from collections import deque
 
 def dfs(x,y):
     nx = [1, 1, -1, -1, 1, -1, 0, 0]
@@ -12,6 +12,22 @@ def dfs(x,y):
             if m[new_x][new_y]==1 and visited[new_x][new_y]==0:
                 visited[new_x][new_y]=1
                 dfs(new_x,new_y)
+def bfs(x,y):
+    nx = [1, 1, -1, -1, 1, -1, 0, 0]
+    ny = [0, 1, 0, 1, -1, -1, 1, -1]
+    q = deque()
+    q.append([x, y]) #큐에 시작좌표 넣어주기
+    while q:
+        a,b=q.popleft() #a,b=x,y
+        for i in range(8):
+            new_x=a+nx[i]
+            new_y=b+ny[i]
+            if 0<=new_x<h and 0<=new_y<w:
+                if m[new_x][new_y]==1:
+                    m[new_x][new_y]=0
+                    q.append([new_x,new_y]) #재귀 대신 큐에 넣어주기 
+        #만약 주변에 남은 섬이 없다면 q.append가 실행되지 않아 큐가 종료된다.(빈 큐)
+                
 
 while(True):
     w,h=map(int,input().split())
@@ -24,7 +40,8 @@ while(True):
     for i in range(h): #행:h 열:w 헷갈림 주의
         for j in range(w):
             if m[i][j]==1 and visited[i][j]==0:
-                dfs(i,j)
+                bfs(i,j)
+                #dfs(i,j)
                 count+=1
     print(count)
 
