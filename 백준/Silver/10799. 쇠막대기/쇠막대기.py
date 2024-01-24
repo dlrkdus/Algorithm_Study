994ccf43ab133d1d636556d332b1ba5a330353ed
dlrkdus <99721126+dlrkdus@users.noreply.github.com>
2024-01-24 19:12:00 +0900

    [Silver II] Title: 쇠막대기, Time: 64 ms, Memory: 31552 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,25 +1,15 @@
-arr=list(map(str,input()))
-count=0
-stick=0
-i=0
-while True:
-    if arr[i]=="(":
-        if arr[i+1]==")": #레이저일 때
-            i+=1
-            count+=stick
-            #print("레이저로 자름(+",stick,"):",count)
-        else: #쇠막대기일 때
-            stick+=1
-    else: #레이저가 아니고 쇠막대기 종료
-        stick-=1
-        count+=1   
-        #print("쇠막대기 종료(+",1,"):",count)
-    i+=1
-    if i>len(arr)-1:
-        break
-    
-
-print(count)
-        
-
-
+ir= input()
+stack=[]
+cnt = 0
+for i in range(len(ir)):
+    if ir[i] == "(":
+        stack.append("(")
+    else :
+        if ir[i-1]=="(":
+            stack.pop()
+            cnt+=len(stack) # 첫 번째 경우인 현재의 쇠막대기들을 카운팅합니다.
+            
+        else :
+            stack.pop()
+            cnt+=1 # 이 부분은 두 번째 경우인 나머지 부분을 세는 것입니다.
+print(cnt)
