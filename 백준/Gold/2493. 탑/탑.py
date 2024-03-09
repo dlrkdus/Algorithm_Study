43bd56ec15f3b0e5b2a0f2c595d58d56aca61465
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-03-09 20:11:24 +0900

    [Gold V] Title: 탑, Time: 712 ms, Memory: 171852 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,19 @@
+n=int(input())
+tower=list(map(int,input().split()))
+
+answer=[]
+stack=[]
+for i in range(n):
+    while stack:
+        if stack[-1][0]>tower[i]:
+            answer.append(stack[-1][1]+1)
+            break
+        else:
+            stack.pop()
+    if not stack:
+        answer.append(0)
+    stack.append([tower[i],i])
+
+    
+print(' '.join(map(str,answer)))
+        
