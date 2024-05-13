4d20460da6061c51b7a4778321541910c3b13315
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-05-13 11:10:21 +0900

    [D2] Title: 고대 유적, Time: 144 ms, Memory: 49,284 KB -BaekjoonHub

--- SWEA/D2/9489. 고대 유적/고대 유적.py
+++ SWEA/D2/9489. 고대 유적/고대 유적.py
@@ -1,33 +1,9 @@
 T=int(input())
 
-def dfs(x,y,count):
-    global max_count
-    visited[x][y]=1
-    if arr[x][y]==0:
-        return 
-    max_count = max(count,max_count)
-
-    nx = [0,0,1,-1]
-    ny = [1,-1,0,0]
-    for i in range(4):
-        new_x = x+nx[i]
-        new_y = y+ny[i]
-        if 0<=new_x<M and 0<=new_y<N and visited[new_x][new_y]==0:
-            dfs(new_x,new_y,count+1)
-    visited[x][y]=0
-
-
 for test_case in range(1,T+1):
     N, M = map(int,input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    visited = [[0 for i in range(M)]for j in range(N)]
 
-    # max_count = 0
-
-    # for i in range(M):
-    #     for j in range(N):
-    #         if arr[i][j]==1 and visited[i][j]==0:
-    #             dfs(i,j,1)
     answer =[]
     for i in range(N):
         count=0
@@ -47,7 +23,5 @@ for test_case in range(1,T+1):
                 answer.append(count)
                 count=0
 
-
-    # print(f"#{test_case}",max_count)
     print(f"#{test_case}",max(answer))
 
