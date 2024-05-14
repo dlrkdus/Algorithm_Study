d8f1b50fc83f187dfeb1eb398390b4d8dcd1e539
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-05-14 23:33:41 +0900

    [D3] Title: N-Queen, Time: 303 ms, Memory: 51,192 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -21,9 +21,12 @@ def dfs(row):
     else:
         for col in range(N): #행별로 어느 열인지, 즉 nq[row]=col이면 row행 col 열에 퀸 위치
             nq[row]=col # 열 위치 지정 (퀸 두기)
-            if check(row): # 다음 행에 퀸 둘 수 있는지 확인, True라면 계속 탐색, False라면 dfs 종료
-                dfs(row+1)
-
+            if check(row): #만약 해당 열에 놓을 수 있다면 다음 행의 열 위치 탐색
+                dfs(row+1) #만약 row 행의 모든 열에 False가 반환된다면 백트래킹으로 이전 dfs(이전 행)로 돌아가 다음 열을 탐색함
+             
+# 즉 행별로 모든 열을 탐색했는데 False라면 이전 행의 열에 놓을 수 없는 것이므로 백트래킹으로 돌아가 
+# 이전 행의 다음 열을 탐색하는 것이다. 
+# 그렇게 계속 돌아가다 초기의 dfs까지 종료되면 count 는 증가 없이 0이 되는 것. 
 
     
 for test_case in range(1,T+1):
@@ -32,5 +35,4 @@ for test_case in range(1,T+1):
     nq = [0]* N #i번째 행의 퀸 열 위치, 행별로 떼서 생각한다.
 
     dfs(0)
-
     print(f"#{test_case}",count)
