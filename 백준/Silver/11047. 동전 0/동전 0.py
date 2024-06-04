287f4601df53ceaa2078bbf7bfbb6f0c9bf42b3b
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-06-04 10:14:48 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,11 +1,14 @@
-N, K = map(int, input().split()) 
-coin_lst = list()
+N, K = map(int,input().split())
+
+c=[]
+result = 0
+
 for i in range(N):
-    coin_lst.append(int(input()))
+    coin = int(input())
+    c.append(coin)
 
-count = 0
 for i in reversed(range(N)):
-    count += K//coin_lst[i] #카운트 값에 K를 동전으로 나눈 몫을 더해줌
-    K = K%coin_lst[i] # K는 동전으로 나눈 나머지로 계속 반복
+    result+= (K//c[i])
+    K%=c[i]
 
-print(count)
+print(result)
