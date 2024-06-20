b248bb17b89ec7a3c0d632ad208207163476bf1b
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-06-20 14:28:16 +0900

    [Silver III] Title: N과 M (1), Time: 1964 ms, Memory: 56248 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+public class Main {
+	static int N,M;
+	static int [] a;
+	static int [] b;
+	static boolean[] v;
+	private static void perm(int cnt) {
+		if(cnt==M) {
+			for (int j = 0; j< b.length; j++) {
+				System.out.print(b[j]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i=0; i<N; i++) {
+			if (v[i]) continue;
+			v[i]=true;
+			b[cnt] = a[i];
+			perm(cnt+1);
+			v[i]=false;
+		}
+		
+	}
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		a = IntStream.range(1, N + 1).toArray();
+		b = new int [M];
+		v = new boolean [N];
+	
+		perm(0);
+		
+
+	}
+
+
+
+}
