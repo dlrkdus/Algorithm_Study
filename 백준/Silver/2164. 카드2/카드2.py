9da6145931ef0bf90a444563f9162641d148e87e
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-06-24 13:24:25 +0900

    [Silver IV] Title: 카드2, Time: 240 ms, Memory: 54992 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,11 +1,13 @@
 from collections import deque
-
 N = int(input())
-deque = deque([i for i in range(1, N+1)])
+d = deque([i for i in range(1,N+1)])
 
-while(len(deque) >1):
-    deque.popleft()
-    tmp = deque.popleft()
-    deque.append(tmp)
-    
-print(deque[0])
+while(True):
+    if len(d)<=1:
+        break
+    d.popleft()
+    if len(d)<=1:
+        break
+    d.append(d.popleft())
+
+print(d[0])
