ca042e9ebbe281ec22f3da96937530a964635c5d
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-06-25 18:28:43 +0900

    [Gold V] Title: 강의실 배정, Time: 496 ms, Memory: 148580 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -8,13 +8,11 @@ for i in range(N):
 
 S.sort()
 hq = [] # 종료 시간을 담을 heapq , h[0]은 가장 이른 종료시간대가 된다
-heapq.heappush(hq,S[0][1]) # 첫번째 종료 시간 저장 
 
-for i in range(1,N):
-    if hq[0]<=S[i][0]: # 시작 시간이 종료 시간보다 뒤라면
-        heapq.heappop(hq) # 한 강의실로 배정
+for i in range(N):
     heapq.heappush(hq, S[i][1]) # 일단 종료시간을 넣어주자
-            
+    if hq[0]<=S[i][0]: # 시작 시간이 종료 시간보다 뒤라면
+        heapq.heappop(hq) # 한 강의실로 배정            
 
 print(len(hq))
 
