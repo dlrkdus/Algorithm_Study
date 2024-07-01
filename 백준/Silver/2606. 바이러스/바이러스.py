0a5dbbe800364484a7cbd4cfbdc98d3b156ea805
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-07-01 16:44:54 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,23 @@
-n=int(input()) #노드
-v=int(input()) #간선
-graph = [[] for _ in range(n+1)]
-visited=[0]*(n+1)
-count=0
-
-for _ in range(v):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
+N = int(input()) #컴퓨터 개수 
+M = int(input()) #컴퓨터 연결 쌍
+computer = [[0 for i in range(N+1)]for j in range(N+1)]
+visited = [0 for i in range(N+1)]
+cnt = 0
+for i in range(M):
+    a,b = map(int,input().split())
+    computer[a][b]=1
+    computer[b][a]=1
 
 def dfs(v):
+    global cnt
     visited[v]=1
-    global count
-    count+=1
-    for i in graph[v]:
-        if visited[i]==0:
-            dfs(i)
+    for i in range(N+1):
+        if computer[v][i] or computer[i][v]:
+            if not visited[i]:
+                cnt+=1
+                dfs(i)
 
 dfs(1)
-print(count-1)
+print(cnt)
+
+
