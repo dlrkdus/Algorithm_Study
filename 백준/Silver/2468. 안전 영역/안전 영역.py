2c4bd80842dceec066b94b0cd1ab25b2adddfc6d
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-08-09 11:27:04 +0900

    [Silver I] Title: 안전 영역, Time: 768 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,39 +1,37 @@
-import copy
-import sys
-sys.setrecursionlimit(10000)
 from collections import deque
+n = int(input())
+arr = []
+d = deque()
+for i in range(n):
+    a = list(map(int,input().split()))
+    arr.append(a)
 
-N = int(input())
-a = [list(map(int,input().split())) for i in range(N)]
-maxA = max(max(row) for row in a)
-minA = min(min(row) for row in a)
-max_area = 0
-nx = [0,0,-1,1]
+max_area = -1
+nx = [0,0,1,-1]
 ny = [1,-1,0,0]
 
-def bfs(i,j,height):
-    d = deque()
-    d.append([i,j])
-
-    while(d):
-        a,b = d.popleft()
-        for x in range (4):
-            new_x = a+nx[x]
-            new_y = b+ny[x]
-            if 0<=new_x<N and 0<=new_y<N:
-                if arr[new_x][new_y]>height:
-                    arr[new_x][new_y]=0
+def bfs(height,a,b):
+    d.append([a,b])
+    while d:
+        x,y = d.popleft()
+        for i in range(4):
+            new_x = x+nx[i]
+            new_y = y+ny[i]
+            if 0<=new_x<n and 0<=new_y<n:
+                if arr[new_x][new_y]>height and not visited[new_x][new_y]:
+                    visited[new_x][new_y]=1
                     d.append([new_x,new_y])
 
-for height in range(maxA+1):
-    count=0
-    arr = copy.deepcopy(a)
-    for i in range(N):
-        for j in range(N):
-            if  arr[i][j]>height:
-                bfs(i,j,height)
-                count+=1
 
+for i in range(max(max(i) for i in arr)):
+    count=0
+    visited = [[0 for i in range(n)]for j in range(n)]
+    for a in range(n):
+        for b in range(n):
+            if arr[a][b]>i and not visited[a][b]:
+                bfs(i,a,b)
+                count+=1
     max_area = max(max_area,count)
 
 print(max_area)
+
