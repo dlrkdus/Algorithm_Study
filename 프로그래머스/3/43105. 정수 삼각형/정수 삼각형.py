19098ffd311076fbccfb21c0bf7100ec09d7e1c0
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-10-30 16:44:56 +0900

    [level 3] Title: 정수 삼각형, Time: 107.82 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -5,9 +5,9 @@ def solution(triangle):
     for i in range(1, len(triangle)):
         for j in range(len(triangle[i])):
             if j == 0:  # 맨 왼쪽 열일 경우
-                dp[i][j] = dp[i-1][j] + triangle[i][j]
+                dp[i][j] = dp[i-1][j] + triangle[i][j] #위 행의 같은 열에게만 받을 수 있음
             elif j == len(triangle[i]) - 1:  # 맨 오른쪽 열일 경우
-                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j] #위 행의 -1열에서만 받을 수 있음
             else:
                 dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
 
