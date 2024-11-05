836f6166b709d53659e2ee9ff2154cf5ecf22b33
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-11-05 14:08:04 +0900

    Update 디스크 컨트롤러.py

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -7,14 +7,15 @@ def solution(jobs):
     
     while i<len(jobs):
         for j in jobs:
-            if start < j[0] <= now:
-                heapq.heappush(hq,[j[1],j[0]])
+            if start < j[0] <= now: # 디스크가 실행되는 동안(start ~ now) 들어온 요청부터 처리
+                heapq.heappush(hq,[j[1],j[0]]) # 실행 시간이 짧은 순으로 처리
         if len(hq) > 0:
             current = heapq.heappop(hq)
             start = now
+            # current[0] = 작업 소요 시간, current[1] = 요청 시각
             now += current[0]
-            sum += (now - current[1])
+            sum += (now - current[1]) # now - current[1] = 요청부터 처리 완료까지의 시간 
             i+=1
         else:
-            now +=1
+            now +=1 # 잉여 시간이면 now 땡기기
     return int(sum/len(jobs))
