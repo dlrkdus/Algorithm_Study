12c242189150a2432bc5b774ef05c725a31f696c
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-11-22 21:52:16 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.57 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -7,7 +7,6 @@ def solution(jobs):
     waitings = []  # 대기열
     jobs.sort(key=lambda x: x[0])  # 요청 시각 기준으로 정렬
     num_jobs = len(jobs)
-    processed_jobs = 0  # 처리한 작업 수
 
     # 프로그램이 모두 처리될 때까지 반복
     while jobs or waitings:
@@ -26,7 +25,6 @@ def solution(jobs):
             duration, request_time = heapq.heappop(waitings)
             now += duration  # 현재 시각을 작업 실행 시간만큼 증가
             total_wait_time += (now - request_time)  # 대기 시간 계산
-            processed_jobs += 1
 
     # 평균 대기 시간 반환
     return total_wait_time // num_jobs
