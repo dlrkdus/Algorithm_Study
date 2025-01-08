f553019fdb632d136f14d7cb4d9b3ccbecad389b
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-01-08 13:53:38 +0900

    [Gold V] Title: 평범한 배낭, Time: 3560 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,15 +1,18 @@
 N, K = map(int,input().split())
-weight = []
-value = []
-dp = [0] *(K+1)
-for _ in range(N):
-    W, V = map(int,input().split())
-    weight.append(W)
-    value.append(V)
+# N = 물건 갯수 K = 최대 무게
+dp = [0]*(K+1) # 무게 당 최대 가치
+weights = []
+values = []
 
-for i in range(N):
-    for j in range(K,0,-1):
-        if weight[i]<=j:
-            dp[j] = max(dp[j],dp[j-weight[i]]+value[i])
+for _ in range(N):
+    w, v = map(int,input().split())
+    weights.append(w)
+    values.append(v)
+
+for i in range(N): # 갯수
+    for j in range(K,0,-1): # 무게
+        if weights[i] <= j: # j = 현재 가방에 남은 무게
+            dp[j] = max(dp[j],dp[j-weights[i]]+values[i])
 
 print(dp[K])
+
