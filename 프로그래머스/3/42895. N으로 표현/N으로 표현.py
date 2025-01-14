e87b5ace137765b53d029af589d079afae0f692e
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-01-14 16:10:57 +0900

    [level 3] Title: N으로 표현, Time: 28.70 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,13 +1,22 @@
+from collections import defaultdict
+
 def solution(N, number):
     if N == number:
         return 1
-    dp = {} # 딕셔너리로 선언
-    dp.setdefault(1, set()).add(N) # value를 집합으로 선언
-    for i in range(2,9):
-        num = int(str(N) * i)
-        dp.setdefault(i, set()).add(num)
+
+    # defaultdict를 사용해 초기값을 자동으로 set()으로 설정
+    dp = defaultdict(set)
+    dp[1].add(N)  # N을 1번 사용한 경우
+
+    # N을 몇 번 사용했는가
+    for i in range(2, 9):
+        num = int(str(N) * i)  # N을 i번 반복한 숫자
+        dp[i].add(num)
+
         # 이전 값들로 새로운 값 생성
         for j in range(1, i):
+		        # dp[j] = N을 j번 사용해서 나올 수 있는 경우
+		        # dp[5] = 1,4 / 2,3 / 3,2/ 4,1의 조합을 모두 구한다. 
             for v1 in dp[j]:
                 for v2 in dp[i - j]:
                     dp[i].add(v1 + v2)
@@ -15,7 +24,9 @@ def solution(N, number):
                     dp[i].add(v1 * v2)
                     if v2 != 0:  # 0으로 나누기 방지
                         dp[i].add(v1 // v2)
-    
+
+        # 원하는 숫자를 찾은 경우
         if number in dp[i]:
             return i
-    return -1
+
+    return -1  # 8번 이하로 만들 수 없는 경우
