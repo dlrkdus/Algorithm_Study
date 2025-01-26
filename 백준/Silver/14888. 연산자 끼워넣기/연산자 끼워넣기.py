0e37b015072bbc8fe12d7bc230b8a976a8d809c7
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-01-26 15:19:28 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 168 ms, Memory: 112252 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,34 +1,32 @@
 N = int(input())
-A = list(map(int,input().split()))
-calc = list(map(int,input().split()))
-sumArr = []
-maxNum = -float('inf')
-minNum = float('inf')
+numbers = list(map(int,input().split()))
+o = list(map(int,input().split()))
+min_num = 10000000000000
+max_num = -1000000000000
 
-def backtrack (level,result):
-    global maxNum,minNum
-    if level == len(A):
-        maxNum = max(maxNum, result)
-        minNum = min(minNum, result)
-        return 
-    for j in range(4):
-        if calc[j]>0:
-            calc[j]-=1
-            if j == 0:
-                backtrack(level+1, result+A[level])
-            if j == 1:
-                backtrack(level+1, result-A[level])
-            if j == 2:
-                backtrack(level+1, result*A[level])
-            if j == 3:
+def dfs(level,result):
+    global min_num,max_num
+    if level == N:
+        min_num = min(min_num,result)
+        max_num = max(max_num,result)
+        return
+    for i in range(4):
+        if o[i] > 0:
+            o[i]-=1
+            if i == 0:
+                dfs(level+1,result+numbers[level])
+            if i == 1:
+                dfs(level+1,result-numbers[level])
+            if i == 2:
+                dfs(level+1,result*numbers[level])
+            if i ==3:
                 if result < 0:
-                    backtrack(level+1, -(-result//A[level]))
+                    dfs(level+1,-(-result//numbers[level]))
                 else:
-                    backtrack(level+1, result//A[level])
-            calc[j]+=1
-            
-backtrack(1, A[0])
-            
-print(maxNum)
-print(minNum)
-                
+                    dfs(level+1,result//numbers[level])
+            o[i]+=1
+
+dfs(1,numbers[0])
+
+print(max_num)
+print(min_num)
