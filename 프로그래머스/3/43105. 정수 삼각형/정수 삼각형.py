acb45275f79e4157ed7866a1e02a151f35a1af8a
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-02-12 12:36:23 +0900

    [level 3] Title: 정수 삼각형, Time: 51.14 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,15 +1,17 @@
 def solution(triangle):
     answer = 0
-    dp = [[0] * len(row) for row in triangle]  # 각 행마다 리스트 생성
+    dp = [[0] * len(row) for row in triangle] 
     dp[0][0] = triangle[0][0]
-    for i in range(1, len(triangle)):
-        for j in range(len(triangle[i])):
-            if j == 0:  # 맨 왼쪽 열일 경우
-                dp[i][j] = dp[i-1][j] + triangle[i][j] #위 행의 같은 열에게만 받을 수 있음
-            elif j == len(triangle[i]) - 1:  # 맨 오른쪽 열일 경우
-                dp[i][j] = dp[i-1][j-1] + triangle[i][j] #위 행의 -1열에서만 받을 수 있음
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
-
+    for i in range(1,len(triangle)):
+        for j in range(i+1):
+            if i>j>0:
+                dp[i][j] = max(dp[i-1][j-1],dp[i-1][j]) + triangle[i][j]
+            if j == 0:
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            if j == i:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
     
-    return max(dp[len(triangle)-1])
+    max_num = -1
+    for row in dp:
+        max_num = max(max(row),max_num)
+    return max_num
