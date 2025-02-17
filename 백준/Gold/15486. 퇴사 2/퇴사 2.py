8dbe79e7b45f96c31d1e6a5fe7490b81c2b18fb3
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-02-17 15:17:49 +0900

    [Gold V] Title: 퇴사 2, Time: 880 ms, Memory: 212360 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,14 +1,17 @@
 N = int(input())
-counsel = []
+T = []
+P = []
 for _ in range(N):
-    counsel.append(list(map(int,input().split())))
-dp = [0] * (N*2)
+    t, p = map(int,input().split())
+    T.append(t)
+    P.append(p)
+dp = [0] * (N+2) # dp[i] = i일 이후로 벌 수 있는 최대 금액
+# 당일 상담을 했을 때와 안 했을 때 max
 
-for i in range(N):
-    # 현재 상담을 진행하지 않고 다음날로 넘어간다면
-    dp[i+1] = max(dp[i+1],dp[i])
-    # 상담이 가능해서 진행한다면
-    if i + counsel[i][0] <= N:
-        dp[i+counsel[i][0]] = max(dp[i+counsel[i][0]], dp[i] + counsel[i][1])
-
+for i in range(N,0,-1):
+    # 상담을 안 했을 때를 기본으로 
+    dp[i] = dp[i+1]
+    # 상담이 가능할 경우
+    if i+T[i-1] <=N+1:
+        dp[i] = max(dp[i],P[i-1]+dp[i+T[i-1]])
 print(max(dp))
