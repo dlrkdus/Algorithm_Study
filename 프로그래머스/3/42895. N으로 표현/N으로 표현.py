611a53871c3d0bfd21bef26230a95dfa6ce78349
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-02-24 13:33:24 +0900

    [level 3] Title: N으로 표현, Time: 112.05 ms, Memory: 14.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,32 +1,28 @@
 from collections import defaultdict
-
+# N을 i번 써서 만들 수 있는 경우를 딕셔너리에 담자.
+# 숫자 N을 최소로 사용해서 number로 만들기
 def solution(N, number):
+    answer = -1
+    dp = defaultdict(set)
+    dp[1].add(N)
     if N == number:
         return 1
-
-    # defaultdict를 사용해 초기값을 자동으로 set()으로 설정
-    dp = defaultdict(set)
-    dp[1].add(N)  # N을 1번 사용한 경우
-
-    # N을 몇 번 사용했는가
-    for i in range(2, 9):
-        num = int(str(N) * i)  # N을 i번 반복한 숫자
-        dp[i].add(num)
-
-        # 이전 값들로 새로운 값 생성
-        for j in range(1, i):
-		        # dp[j] = N을 j번 사용해서 나올 수 있는 경우
-		        # dp[5] = 1,4 / 2,3 / 3,2/ 4,1의 조합을 모두 구한다. 
+    for i in range(2,10):
+        dp[i].add(int(str(N)*i)) # 55, 555,,
+        for j in range(1, i): # 이전거랑 조합해야 함
             for v1 in dp[j]:
-                for v2 in dp[i - j]:
-                    dp[i].add(v1 + v2)
-                    dp[i].add(v1 - v2)
-                    dp[i].add(v1 * v2)
-                    if v2 != 0:  # 0으로 나누기 방지
-                        dp[i].add(v1 // v2)
+                for v2 in dp[i-j]:
+                    dp[i].add(v1+v2)
+                    dp[i].add(v1-v2)
+                    dp[i].add(v1*v2)
+                    if v2!=0:
+                        dp[i].add(v1//v2)
 
-        # 원하는 숫자를 찾은 경우
+    for i in range(1,10):
         if number in dp[i]:
-            return i
+            if i<=8:
+                return i
+    return answer
+        
 
-    return -1  # 8번 이하로 만들 수 없는 경우
+    
